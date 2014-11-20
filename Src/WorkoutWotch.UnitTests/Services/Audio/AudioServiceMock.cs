12750c7cbb4286d6740d247ed70eadc399255db1
Kent Boogaart <kent.boogaart@gmail.com>
2014-11-21 08:33:49 +1030

    Add AudioServiceMock.

---
+++ Src/WorkoutWotch.UnitTests/Services/Audio/AudioServiceMock.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkoutWotch.UnitTests.Services.Audio
+{
+    using System.Threading.Tasks;
+    using Kent.Boogaart.PCLMock;
+    using WorkoutWotch.Services.Contracts.Audio;
+
+    public sealed class AudioServiceMock : MockBase<IAudioService>, IAudioService
+    {
+        public AudioServiceMock(MockBehavior behavior = MockBehavior.Strict)
+            : base(behavior)
+        {
+            if (behavior == MockBehavior.Loose)
+            {
+                this.When(x => x.PlayAsync(It.IsAny<string>())).Return(Task.FromResult(true));
+            }
+        }
+
+        public Task PlayAsync(string resourceUri)
+        {
+            return this.Apply(x => x.PlayAsync(resourceUri));
+        }
+    }
+}
