b9b8231a8013b653b1c77e7f2de1d3cb34eb325a
Kent Boogaart <kent.boogaart@gmail.com>
2014-11-21 08:38:12 +1030

    Add SpeechServiceMock.

---
+++ Src/WorkoutWotch.UnitTests/Services/Speech/SpeechServiceMock.cs
@@ -0,0 +1,24 @@
+﻿namespace WorkoutWotch.UnitTests.Services.Speech
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Kent.Boogaart.PCLMock;
+    using WorkoutWotch.Services.Contracts.Speech;
+
+    public sealed class SpeechServiceMock : MockBase<ISpeechService>, ISpeechService
+    {
+        public SpeechServiceMock(MockBehavior behavior = MockBehavior.Strict)
+            : base(behavior)
+        {
+            if (behavior == MockBehavior.Loose)
+            {
+                this.When(x => x.SpeakAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).Return(Task.FromResult(true));
+            }
+        }
+
+        public Task SpeakAsync(string speechString, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return this.Apply(x => x.SpeakAsync(speechString, cancellationToken));
+        }
+    }
+}
