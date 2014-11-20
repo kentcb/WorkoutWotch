712d9d13a58eea6d7a54ceff3563fbe40bfba77b
Kent Boogaart <kent.boogaart@gmail.com>
2014-11-20 10:55:19 +1030

    Add ExerciseDocumentServiceMock.

---
+++ Src/WorkoutWotch.UnitTests/Services/ExerciseDocument/Mocks/ExerciseDocumentServiceMock.cs
@@ -0,0 +1,24 @@
+﻿namespace WorkoutWotch.UnitTests.Services.ExerciseDocument.Mocks
+{
+    using System;
+    using System.Reactive.Linq;
+    using Kent.Boogaart.PCLMock;
+    using WorkoutWotch.Services.Contracts.ExerciseDocument;
+
+    public sealed class ExerciseDocumentServiceMock : MockBase<IExerciseDocumentService>, IExerciseDocumentService
+    {
+        public ExerciseDocumentServiceMock(MockBehavior behavior = MockBehavior.Strict)
+            : base(behavior)
+        {
+            if (behavior == MockBehavior.Loose)
+            {
+                this.When(x => x.ExerciseDocument).Return(Observable.Empty<string>());
+            }
+        }
+
+        public IObservable<string> ExerciseDocument
+        {
+            get { return this.Apply(x => x.ExerciseDocument); }
+        }
+    }
+}
