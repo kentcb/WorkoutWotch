34c74c4af89b280ef0c4a1dcea7f3104dfeae0c8
Kent Boogaart <kent.boogaart@gmail.com>
2014-11-26 10:29:36 +1030

    Module 05, Video 02: Model Core Abstractions: IAction

---
+++ Src/WorkoutWotch.Models/ExecutionContext.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Threading.Tasks;
+
+namespace WorkoutWotch.Models
+{
+	public class ExecutionContext
+	{
+	}
+
+}
+
---
+++ Src/WorkoutWotch.Models/IAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WorkoutWotch.Models
+{
+    public interface IAction
+    {
+        TimeSpan Duration
+        {
+            get;
+        }
+
+        Task ExecuteAsync(ExecutionContext context);
+    }
+}
+
