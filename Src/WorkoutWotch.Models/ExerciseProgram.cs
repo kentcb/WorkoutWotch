57918ed1d955414b819fff23a0c1507360a94d42
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-01 09:13:27 +1030

    Module 05, Video 07: Model: ExerciseProgram

---
+++ Src/WorkoutWotch.Models/ExerciseProgram.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using Kent.Boogaart.HelperTrinity.Extensions;
+using System.Linq;
+
+namespace WorkoutWotch.Models
+{
+    public sealed class ExerciseProgram
+    {
+        private readonly string name;
+        private readonly IImmutableList<Exercise> exercises;
+        private readonly TimeSpan duration;
+
+        public ExerciseProgram(string name, IEnumerable<Exercise> exercises)
+        {
+            name.AssertNotNull("name");
+            exercises.AssertNotNull("exercises", assertContentsNotNull: true);
+
+            this.name = name;
+            this.exercises = exercises.ToImmutableList();
+            this.duration = this
+                .exercises
+                .Select(x => x.Duration)
+                .DefaultIfEmpty()
+                .Aggregate((running, next) => running + next);
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public TimeSpan Duration
+        {
+            get { return this.duration; }
+        }
+
+        public IImmutableList<Exercise> Exercises
+        {
+            get { return this.exercises; }
+        }
+
+        public async Task ExecuteAsync(ExecutionContext context)
+        {
+            context.AssertNotNull("context");
+
+            foreach (var exercise in this.exercises)
+            {
+                if (context.SkipAhead > TimeSpan.Zero && context.SkipAhead >= exercise.Duration)
+                {
+                    // we can skip the exercise
+                    context.AddProgress(exercise.Duration);
+                    continue;
+                }
+
+                await exercise.ExecuteAsync(context).ContinueOnAnyContext();
+            }
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/ExerciseProgramFixture.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using WorkoutWotch.Models;
+using Kent.Boogaart.PCLMock;
+using WorkoutWotch.UnitTests.Services.Logger.Mocks;
+using System.Collections.Generic;
+using WorkoutWotch.UnitTests.Models.Mocks;
+using WorkoutWotch.Models.Events;
+using System.Threading.Tasks;
+
+namespace WorkoutWotch.UnitTests.Models
+{
+    [TestFixture]
+    public class ExerciseProgramFixture
+    {
+        [Test]
+        public void ctor_throws_if_name_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ExerciseProgram(null, Enumerable.Empty<Exercise>()));
+        }
+
+        [Test]
+        public void ctor_throws_if_exercises_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ExerciseProgram("name", null));
+        }
+
+        [Test]
+        public void ctor_throws_if_any_exercise_is_null()
+        {
+            Assert.Throws<ArgumentException>(() => new ExerciseProgram("name", new Exercise[] { this.CreateExercise("name"), null }));
+        }
+
+        [Test]
+        public void name_returns_value_provided_to_ctor()
+        {
+            Assert.AreEqual("some name", new ExerciseProgram("some name", Enumerable.Empty<Exercise>()).Name);
+        }
+
+        [Test]
+        public void exercises_returns_exercises_provided_to_ctor()
+        {
+            var exercises = new List<Exercise>
+            {
+                this.CreateExercise("first"),
+                this.CreateExercise("second"),
+                this.CreateExercise("third")
+            };
+            var exerciseProgram = new ExerciseProgram("name", exercises);
+
+            Assert.AreEqual(3, exerciseProgram.Exercises.Count);
+            Assert.AreSame(exercises[0], exerciseProgram.Exercises[0]);
+            Assert.AreSame(exercises[1], exerciseProgram.Exercises[1]);
+            Assert.AreSame(exercises[2], exerciseProgram.Exercises[2]);
+        }
+
+        [Test]
+        public void duration_is_zero_if_there_are_no_exercises()
+        {
+            Assert.AreEqual(TimeSpan.Zero, new ExerciseProgram("name", Enumerable.Empty<Exercise>()).Duration);
+        }
+
+        [Test]
+        public void duration_is_the_sum_of_all_exercise_durations()
+        {
+            var action1 = new ActionMock();
+            var action2 = new ActionMock();
+            action1.When(x => x.Duration).Return(TimeSpan.FromSeconds(1));
+            action2.When(x => x.Duration).Return(TimeSpan.FromSeconds(10));
+            var exercises = new []
+            {
+                this.CreateExercise("first", action1),
+                this.CreateExercise("second", action2),
+                this.CreateExercise("third", action1)
+            };
+            var exerciseProgram = new ExerciseProgram("name", exercises);
+
+            Assert.AreEqual(TimeSpan.FromSeconds(12), exerciseProgram.Duration);
+        }
+
+        [Test]
+        public void execute_async_throws_if_the_context_is_null()
+        {
+            var exerciseProgram = new ExerciseProgram("name", Enumerable.Empty<Exercise>());
+            Assert.Throws<ArgumentNullException>(async () => await exerciseProgram.ExecuteAsync(null));
+        }
+
+        [Test]
+        public async Task execute_async_executes_each_exercise()
+        {
+            var action1 = new ActionMock(MockBehavior.Loose);
+            var action2 = new ActionMock(MockBehavior.Loose);
+            action1.When(x => x.Duration).Return(TimeSpan.FromSeconds(1));
+            action2.When(x => x.Duration).Return(TimeSpan.FromSeconds(10));
+            var exercises = new []
+            {
+                this.CreateExercise("first", action1),
+                this.CreateExercise("second", action2)
+            };
+            var exerciseProgram = new ExerciseProgram("name", exercises);
+
+            using (var executionContext = new ExecutionContext())
+            {
+                await exerciseProgram.ExecuteAsync(executionContext);
+
+                action1.Verify(x => x.ExecuteAsync(It.Is(executionContext))).WasCalledExactlyOnce();
+                action2.Verify(x => x.ExecuteAsync(It.Is(executionContext))).WasCalledExactlyOnce();
+            }
+        }
+
+        [Test]
+        public async Task execute_async_skips_exercises_that_are_shorter_than_the_skip_ahead()
+        {
+            var action1 = new ActionMock(MockBehavior.Loose);
+            var action2 = new ActionMock(MockBehavior.Loose);
+            var action3 = new ActionMock(MockBehavior.Loose);
+            action1.When(x => x.Duration).Return(TimeSpan.FromSeconds(13));
+            action2.When(x => x.Duration).Return(TimeSpan.FromSeconds(10));
+            action3.When(x => x.Duration).Return(TimeSpan.FromSeconds(5));
+            action1.When(x => x.ExecuteAsync(It.IsAny<ExecutionContext>())).Throw();
+            action2.When(x => x.ExecuteAsync(It.IsAny<ExecutionContext>())).Throw();
+            var exercises = new []
+            {
+                this.CreateExercise("first", action1),
+                this.CreateExercise("second", action2),
+                this.CreateExercise("third", action3)
+            };
+            var exerciseProgram = new ExerciseProgram("name", exercises);
+
+            using (var executionContext = new ExecutionContext(TimeSpan.FromSeconds(23)))
+            {
+                await exerciseProgram.ExecuteAsync(executionContext);
+
+                action3.Verify(x => x.ExecuteAsync(It.Is(executionContext))).WasCalledExactlyOnce();
+            }
+        }
+
+        #region Supporting Members
+
+        private Exercise CreateExercise(string name, IAction beforeExerciseAction = null)
+        {
+            var matchers = new List<MatcherWithAction>();
+
+            if (beforeExerciseAction != null)
+            {
+                var matcher = new EventMatcherMock();
+                matcher.When(x => x.Matches(It.IsAny<IEvent>())).Return((IEvent @event) => @event is BeforeExerciseEvent);
+                matchers.Add(new MatcherWithAction(matcher, beforeExerciseAction));
+            }
+
+            return new Exercise(new LoggerServiceMock(MockBehavior.Loose), name, 0, 0, matchers);
+        }
+
+        #endregion
+    }
+}
