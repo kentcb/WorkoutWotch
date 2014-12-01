1a5a4b74bbddd6e097b8025a36c6af05c4c4dded
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-02 10:08:59 +1030

    Module 05, Video 08: Model: ExercisePrograms

---
+++ Src/WorkoutWotch.Models/ExercisePrograms.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models
+{
+    public sealed class ExercisePrograms
+    {
+        private readonly IImmutableList<ExerciseProgram> programs;
+
+        public ExercisePrograms(IEnumerable<ExerciseProgram> programs)
+        {
+            programs.AssertNotNull("programs", assertContentsNotNull: true);
+            this.programs = programs.ToImmutableList();
+        }
+
+        public IImmutableList<ExerciseProgram> Programs
+        {
+            get { return this.programs; }
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/ExerciseProgramsFixture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.Models;
+using Kent.Boogaart.PCLMock;
+using System.Linq;
+using WorkoutWotch.UnitTests.Services.Logger.Mocks;
+
+namespace WorkoutWotch.UnitTests.Models
+{
+    [TestFixture]
+    public class ExerciseProgramsFixture
+    {
+        [Test]
+        public void ctor_throws_if_programs_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ExercisePrograms(null));
+        }
+
+        [Test]
+        public void ctor_throws_if_any_program_is_null()
+        {
+            Assert.Throws<ArgumentException>(() => new ExercisePrograms(
+                new []
+                {
+                    new ExerciseProgram(new LoggerServiceMock(MockBehavior.Loose), "first", Enumerable.Empty<Exercise>()),
+                    null
+                }));
+        }
+
+        [Test]
+        public void programs_contains_programs_passed_into_ctor()
+        {
+            var programs = new[]
+            {
+                new ExerciseProgram(new LoggerServiceMock(MockBehavior.Loose), "first", Enumerable.Empty<Exercise>()),
+                new ExerciseProgram(new LoggerServiceMock(MockBehavior.Loose), "second", Enumerable.Empty<Exercise>()),
+                new ExerciseProgram(new LoggerServiceMock(MockBehavior.Loose), "third", Enumerable.Empty<Exercise>())
+            };
+            var exercisePrograms = new ExercisePrograms(programs);
+
+            Assert.NotNull(exercisePrograms.Programs);
+            Assert.AreSame(programs[0], exercisePrograms.Programs[0]);
+            Assert.AreSame(programs[1], exercisePrograms.Programs[1]);
+            Assert.AreSame(programs[2], exercisePrograms.Programs[2]);
+        }
+    }
+}
+
