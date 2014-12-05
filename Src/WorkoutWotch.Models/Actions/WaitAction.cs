37f1109e48ae781b918519954ebbebd530034830
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-05 18:15:51 +1030

    Module 05, Video 11: Model: WaitAction

---
+++ Src/WorkoutWotch.Models/Actions/WaitAction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using WorkoutWotch.Services.Contracts.Delay;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models.Actions
+{
+    public sealed class WaitAction : IAction
+    {
+        private static readonly TimeSpan maximumDelayTime = TimeSpan.FromSeconds(1);
+        private readonly IDelayService delayService;
+        private readonly TimeSpan delay;
+
+        public WaitAction(IDelayService delayService, TimeSpan delay)
+        {
+            delayService.AssertNotNull("delayService");
+
+            if (delay < TimeSpan.Zero)
+            {
+                throw new ArgumentException("delay must be greater than or equal to zero.", "delay");
+            }
+
+            this.delayService = delayService;
+            this.delay = delay;
+        }
+
+        public TimeSpan Duration
+        {
+            get { return this.delay; }
+        }
+
+        public async Task ExecuteAsync(ExecutionContext context)
+        {
+            context.AssertNotNull("context");
+
+            var remainingDelay = this.delay;
+
+            if (context.SkipAhead > TimeSpan.Zero)
+            {
+                remainingDelay = remainingDelay - context.SkipAhead;
+
+                if (remainingDelay < TimeSpan.Zero)
+                {
+                    remainingDelay = TimeSpan.Zero;
+                }
+
+                context.AddProgress(context.SkipAhead);
+            }
+
+            while (remainingDelay > TimeSpan.Zero)
+            {
+                context.CancellationToken.ThrowIfCancellationRequested();
+                await context.WaitWhilePausedAsync().ContinueOnAnyContext();
+
+                var delayFor = remainingDelay > maximumDelayTime ? maximumDelayTime : remainingDelay;
+                await this.delayService.DelayAsync(delayFor, context.CancellationToken).ContinueOnAnyContext();
+
+                remainingDelay -= delayFor;
+                context.AddProgress(delayFor);
+            }
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Actions/WaitActionFixture.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.Models.Actions;
+using WorkoutWotch.UnitTests.Services.Delay;
+using System.Threading.Tasks;
+using Kent.Boogaart.PCLMock;
+using System.Threading;
+using WorkoutWotch.Models;
+using ReactiveUI;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+namespace WorkoutWotch.UnitTests.Models.Actions
+{
+    [TestFixture]
+    public class WaitActionFixture
+    {
+        [Test]
+        public void ctor_throws_if_delay_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new WaitAction(null, TimeSpan.Zero));
+        }
+
+        [Test]
+        public void ctor_throws_if_delay_is_less_than_zero()
+        {
+            Assert.Throws<ArgumentException>(() => new WaitAction(new DelayServiceMock(), TimeSpan.FromSeconds(-1)));
+        }
+
+        [Test]
+        public void duration_returns_delay()
+        {
+            var waitAction = new WaitAction(new DelayServiceMock(), TimeSpan.FromSeconds(23));
+            Assert.AreEqual(TimeSpan.FromSeconds(23), waitAction.Duration);
+        }
+
+        [Test]
+        public void execute_async_throws_if_context_is_null()
+        {
+            var waitAction = new WaitAction(new DelayServiceMock(), TimeSpan.Zero);
+            Assert.Throws<ArgumentNullException>(async () => await waitAction.ExecuteAsync(null));
+        }
+
+        [Test]
+        public async Task execute_async_breaks_for_the_specified_delay()
+        {
+            var delayService = new DelayServiceMock();
+            var totalDelay = TimeSpan.Zero;
+            delayService
+                .When(x => x.DelayAsync(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                .Do<TimeSpan, CancellationToken>((t, ct) => totalDelay += t)
+                .Return(Task.FromResult(true));
+            var waitAction = new WaitAction(delayService, TimeSpan.FromMilliseconds(850));
+
+            await waitAction.ExecuteAsync(new ExecutionContext());
+
+            Assert.AreEqual(TimeSpan.FromMilliseconds(850), totalDelay);
+        }
+
+        [Test]
+        public async Task execute_async_skips_ahead_if_the_context_has_skip_ahead()
+        {
+            var delayService = new DelayServiceMock();
+            var totalDelay = TimeSpan.Zero;
+            delayService
+                .When(x => x.DelayAsync(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                .Do<TimeSpan, CancellationToken>((t, ct) => totalDelay += t)
+                .Return(Task.FromResult(true));
+            var waitAction = new WaitAction(delayService, TimeSpan.FromMilliseconds(850));
+
+            await waitAction.ExecuteAsync(new ExecutionContext(TimeSpan.FromMilliseconds(800)));
+
+            Assert.AreEqual(TimeSpan.FromMilliseconds(50), totalDelay);
+        }
+
+        [Test]
+        public async Task execute_async_skips_ahead_if_the_context_has_skip_ahead_even_if_the_context_is_paused()
+        {
+            var delayService = new DelayServiceMock();
+            var totalDelay = TimeSpan.Zero;
+            delayService
+                .When(x => x.DelayAsync(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                .Do<TimeSpan, CancellationToken>((t, ct) => totalDelay += t)
+                .Return(Task.FromResult(true));
+            var waitAction = new WaitAction(delayService, TimeSpan.FromMilliseconds(850));
+
+            using (var context = new ExecutionContext(TimeSpan.FromMilliseconds(800)) { IsPaused = true })
+            {
+                var task = waitAction.ExecuteAsync(context);
+
+                Assert.False(task.Wait(TimeSpan.FromMilliseconds(50)));
+
+                await context
+                    .WhenAnyValue(x => x.Progress)
+                    .Where(x => x == TimeSpan.FromMilliseconds(800))
+                    .FirstAsync()
+                    .Timeout(TimeSpan.FromSeconds(3))
+                    .ToTask();
+            }
+        }
+
+        [Test]
+        public async Task execute_async_reports_progress()
+        {
+            var delayService = new DelayServiceMock(MockBehavior.Loose);
+            var waitAction = new WaitAction(delayService, TimeSpan.FromMilliseconds(50));
+
+            using (var context = new ExecutionContext())
+            {
+                Assert.AreEqual(TimeSpan.Zero, context.Progress);
+
+                await waitAction.ExecuteAsync(context);
+
+                Assert.AreEqual(TimeSpan.FromMilliseconds(50), context.Progress);
+            }
+        }
+
+        [Test]
+        public async Task execute_async_bails_out_if_context_is_cancelled()
+        {
+            var delayService = new DelayServiceMock();
+            var delayCallCount = 0;
+
+            using (var context = new ExecutionContext())
+            {
+                delayService
+                    .When(x => x.DelayAsync(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                    .Do(
+                        () =>
+                        {
+                            if (delayCallCount++ == 2)
+                            {
+                                context.Cancel();
+                            }
+                        })
+                    .Return(Task.FromResult(true));
+
+                var waitAction = new WaitAction(delayService, TimeSpan.FromSeconds(50));
+
+                try
+                {
+                    await waitAction.ExecuteAsync(context);
+                    Assert.Fail("Expecting operation cancelled exception.");
+                }
+                catch (OperationCanceledException)
+                {
+                }
+
+                Assert.True(context.IsCancelled);
+            }
+        }
+
+        [Test]
+        public async Task execute_async_pauses_if_context_is_paused()
+        {
+            var delayService = new DelayServiceMock();
+            var delayCallCount = 0;
+
+            using (var context = new ExecutionContext())
+            {
+                delayService
+                    .When(x => x.DelayAsync(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                    .Do(
+                        () =>
+                        {
+                            if (delayCallCount++ == 2)
+                            {
+                                context.IsPaused = true;
+                            }
+                        })
+                    .Return(Task.FromResult(true));
+
+                var waitAction = new WaitAction(delayService, TimeSpan.FromSeconds(50));
+
+                var task = waitAction.ExecuteAsync(context);
+                Assert.False(task.Wait(TimeSpan.FromMilliseconds(50)));
+
+                await context
+                    .WhenAnyValue(x => x.IsPaused)
+                    .Where(x => x)
+                    .FirstAsync()
+                    .Timeout(TimeSpan.FromSeconds(3))
+                    .ToTask();
+
+                Assert.True(context.IsPaused);
+            }
+        }
+    }
+}
+
