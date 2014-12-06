1d33ee2f2cad0ad86073ffd389b4efddb75b47c3
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-06 14:38:16 +1030

    Module 05, Video 13: Model: AudioAction

---
+++ Src/WorkoutWotch.Models/Actions/AudioAction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using WorkoutWotch.Services.Contracts.Audio;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models.Actions
+{
+    public sealed class AudioAction : IAction
+    {
+        private readonly IAudioService audioService;
+        private readonly string audioResourceUri;
+
+        public AudioAction(IAudioService audioService, string audioResourceUri)
+        {
+            audioService.AssertNotNull("audioService");
+            audioResourceUri.AssertNotNull("audioResourceUri");
+
+            this.audioService = audioService;
+            this.audioResourceUri = audioResourceUri;
+        }
+
+        public TimeSpan Duration
+        {
+            get { return TimeSpan.Zero; }
+        }
+
+        public async Task ExecuteAsync(ExecutionContext context)
+        {
+            context.AssertNotNull("context");
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            await context
+                .WaitWhilePausedAsync()
+                .ContinueOnAnyContext();
+
+            await this.audioService
+                .PlayAsync(this.audioResourceUri)
+                .ContinueOnAnyContext();
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Actions/AudioActionFixture.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.Models.Actions;
+using WorkoutWotch.UnitTests.Services.Audio.Mocks;
+using System.Threading.Tasks;
+using WorkoutWotch.Models;
+using Kent.Boogaart.PCLMock;
+using System.Threading;
+
+namespace WorkoutWotch.UnitTests.Models.Actions
+{
+    [TestFixture]
+    public class AudioActionFixture
+    {
+        [Test]
+        public void ctor_throws_if_audio_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AudioAction(null, "some_uri"));
+        }
+
+        [Test]
+        public void ctor_throws_if_audio_resource_uri_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AudioAction(new AudioServiceMock(), null));
+        }
+
+        [Test]
+        public void duration_returns_zero()
+        {
+            Assert.AreEqual(TimeSpan.Zero, new AudioAction(new AudioServiceMock(), "some_uri").Duration);
+        }
+
+        [Test]
+        public void execute_async_throws_if_context_is_null()
+        {
+            var sut = new AudioAction(new AudioServiceMock(), "some_uri");
+            Assert.Throws<ArgumentNullException>(async () => await sut.ExecuteAsync(null));
+        }
+
+        [Test]
+        public async Task execute_async_cancels_if_context_is_cancelled()
+        {
+            var sut = new AudioAction(new AudioServiceMock(), "some_uri");
+
+            using (var context = new ExecutionContext())
+            {
+                context.Cancel();
+
+                try
+                {
+                    await sut.ExecuteAsync(context);
+                    Assert.Fail("Expecting operation canceled exception.");
+                }
+                catch (OperationCanceledException)
+                {
+                }
+            }
+        }
+
+        [Test]
+        public void execute_async_pauses_if_context_is_paused()
+        {
+            var sut = new AudioAction(new AudioServiceMock(), "some_uri");
+
+            using (var context = new ExecutionContext())
+            {
+                context.IsPaused = true;
+
+                var task = sut.ExecuteAsync(context);
+
+                Assert.False(task.Wait(TimeSpan.FromMilliseconds(50)));
+            }
+        }
+
+        [Test]
+        public async Task execute_async_passes_the_audio_resource_uri_onto_the_audio_service()
+        {
+            var audioService = new AudioServiceMock(MockBehavior.Loose);
+            var sut = new AudioAction(audioService, "some_uri");
+
+            await sut.ExecuteAsync(new ExecutionContext());
+
+            audioService.Verify(x => x.PlayAsync(It.Is("some_uri"))).WasCalledExactlyOnce();
+        }
+    }
+}
+
