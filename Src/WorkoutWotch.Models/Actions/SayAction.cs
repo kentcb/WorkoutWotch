a95de59f5b687b0803b5b462f16a9b79c67da4cc
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-06 11:07:30 +1030

    Module 05, Video 12: Model: SayAction

---
+++ Src/WorkoutWotch.Models/Actions/SayAction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using WorkoutWotch.Services.Contracts.Speech;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models.Actions
+{
+    public sealed class SayAction : IAction
+    {
+        private readonly ISpeechService speechService;
+        private readonly string speechText;
+
+        public SayAction(ISpeechService speechService, string speechText)
+        {
+            speechService.AssertNotNull("speechService");
+            speechText.AssertNotNull("speechText");
+
+            this.speechService = speechService;
+            this.speechText = speechText;
+        }
+
+        public TimeSpan Duration
+        {
+            get { return TimeSpan.Zero; }
+        }
+
+        public async Task ExecuteAsync(ExecutionContext context)
+        {
+            context.AssertNotNull("context");
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            await context
+                .WaitWhilePausedAsync()
+                .ContinueOnAnyContext();
+
+            await this.speechService
+                .SpeakAsync(this.speechText, context.CancellationToken)
+                .ContinueOnAnyContext();
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Actions/SayActionFixture.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.Models.Actions;
+using WorkoutWotch.UnitTests.Services.Speech.Mocks;
+using WorkoutWotch.Models;
+using System.Threading.Tasks;
+using Kent.Boogaart.PCLMock;
+using System.Threading;
+
+namespace WorkoutWotch.UnitTests.Models.Actions
+{
+    [TestFixture]
+    public class SayActionFixture
+    {
+        [Test]
+        public void ctor_throws_if_speech_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SayAction(null, "something to say"));
+        }
+
+        [Test]
+        public void ctor_throws_if_speech_text_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SayAction(new SpeechServiceMock(), null));
+        }
+
+        [Test]
+        public void duration_returns_zero()
+        {
+            Assert.AreEqual(TimeSpan.Zero, new SayAction(new SpeechServiceMock(), "something to say").Duration);
+        }
+
+        [Test]
+        public void execute_async_throws_if_context_is_null()
+        {
+            var sut = new SayAction(new SpeechServiceMock(), "something to say");
+            Assert.Throws<ArgumentNullException>(async () => await sut.ExecuteAsync(null));
+        }
+
+        [Test]
+        public async Task execute_async_cancels_if_context_is_cancelled()
+        {
+            var sut = new SayAction(new SpeechServiceMock(), "something to say");
+
+            using (var context = new ExecutionContext())
+            {
+                context.Cancel();
+
+                try
+                {
+                    await sut.ExecuteAsync(context);
+                    Assert.Fail("Expecting operation canceled exception.");
+                }
+                catch (OperationCanceledException)
+                {
+                }
+            }
+        }
+
+        [Test]
+        public void execute_async_pauses_if_context_is_paused()
+        {
+            var sut = new SayAction(new SpeechServiceMock(), "something to say");
+
+            using (var context = new ExecutionContext())
+            {
+                context.IsPaused = true;
+
+                var task = sut.ExecuteAsync(context);
+
+                Assert.False(task.Wait(TimeSpan.FromMilliseconds(50)));
+            }
+        }
+
+        [Test]
+        public async Task execute_async_passes_the_speech_text_onto_the_speech_service()
+        {
+            var speechService = new SpeechServiceMock(MockBehavior.Loose);
+            var sut = new SayAction(speechService, "something to say");
+
+            await sut.ExecuteAsync(new ExecutionContext());
+
+            speechService.Verify(x => x.SpeakAsync(It.Is("something to say"), It.IsAny<CancellationToken>())).WasCalledExactlyOnce();
+        }
+    }
+}
+
