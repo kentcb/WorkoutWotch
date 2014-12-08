1d671242038a4f13a72c15d560a7bf53359d920a
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-08 18:01:10 +1030

    Module 05, Video 17: Model: NumberedEventMatcher

---
+++ Src/WorkoutWotch.Models/EventMatchers/NumberedEventMatcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WorkoutWotch.Models.Events;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models.EventMatchers
+{
+    public sealed class NumberedEventMatcher<T> : IEventMatcher
+        where T : NumberedEvent
+    {
+        private readonly Func<T, bool> matches;
+
+        public NumberedEventMatcher(Func<T, bool> matches)
+        {
+            matches.AssertNotNull("matches");
+            this.matches = matches;
+        }
+
+        public bool Matches(IEvent @event)
+        {
+            var castEvent = @event as T;
+
+            if (castEvent == null)
+            {
+                return false;
+            }
+
+            return this.matches(castEvent);
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/EventMatchers/NumberedEventMatcherFixture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.Models.EventMatchers;
+using WorkoutWotch.Models.Events;
+using WorkoutWotch.Models;
+
+namespace WorkoutWotch.UnitTests.Models.EventMatchers
+{
+    [TestFixture]
+    public class NumberedEventMatcherFixture
+    {
+        [Test]
+        public void ctor_throws_if_matches_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new NumberedEventMatcher<BeforeSetEvent>(null));
+        }
+
+        [Test]
+        public void matches_returns_false_if_event_type_differs()
+        {
+            var sut = new NumberedEventMatcher<BeforeSetEvent>(_ => true);
+            Assert.False(sut.Matches(new AfterSetEvent(new ExecutionContext(), 1)));
+        }
+
+        [Test]
+        public void matches_invokes_matches_function_to_obtain_result()
+        {
+            var called = false;
+            Func<BeforeSetEvent, bool> matches = x =>
+            {
+                called = true;
+                return true;
+            };
+            var sut = new NumberedEventMatcher<BeforeSetEvent>(matches);
+            Assert.True(sut.Matches(new BeforeSetEvent(new ExecutionContext(), 1)));
+            Assert.True(called);
+        }
+    }
+}
+
