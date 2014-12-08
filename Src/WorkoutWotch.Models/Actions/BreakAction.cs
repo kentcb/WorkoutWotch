7e5e75ae5b3095fa1fee44136123de94dc53e0c7
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-08 14:13:31 +1030

    Module 05, Video 15: Model: BreakAction

---
+++ Src/WorkoutWotch.Models/Actions/BreakAction.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using WorkoutWotch.Services.Contracts.Speech;
+using WorkoutWotch.Services.Contracts.Delay;
+using Kent.Boogaart.HelperTrinity.Extensions;
+using System.Collections.Generic;
+
+namespace WorkoutWotch.Models.Actions
+{
+    public sealed class BreakAction : IAction
+    {
+        private static readonly TimeSpan minimumBreakToIncludeReady = TimeSpan.FromSeconds(2);
+        private readonly IDelayService delayService;
+        private readonly ISpeechService speechService;
+        private readonly TimeSpan duration;
+
+        public BreakAction(IDelayService delayService, ISpeechService speechService, TimeSpan duration)
+        {
+            delayService.AssertNotNull("delayService");
+            speechService.AssertNotNull("speechService");
+
+            if (duration < TimeSpan.Zero)
+            {
+                throw new ArgumentException("duration must be greater than or equal to zero.", "duration");
+            }
+
+            this.delayService = delayService;
+            this.speechService = speechService;
+            this.duration = duration;
+        }
+
+        public TimeSpan Duration
+        {
+            get { return this.duration; }
+        }
+
+        public async Task ExecuteAsync(ExecutionContext context)
+        {
+            context.AssertNotNull("context");
+
+            foreach (var innerAction in this.GetInnerActions())
+            {
+                await innerAction
+                    .ExecuteAsync(context)
+                    .ContinueOnAnyContext();
+            }
+        }
+
+        private IEnumerable<IAction> GetInnerActions()
+        {
+            yield return new SayAction(this.speechService, "break");
+
+            if (this.duration < minimumBreakToIncludeReady)
+            {
+                yield return new WaitAction(this.delayService, this.duration);
+            }
+            else
+            {
+                yield return new WaitAction(this.delayService, this.duration - minimumBreakToIncludeReady);
+                yield return new SayAction(this.speechService, "ready?");
+                yield return new WaitAction(this.delayService, minimumBreakToIncludeReady);
+            }
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Actions/BreakActionFixture.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.UnitTests.Services.Speech.Mocks;
+using WorkoutWotch.Models.Actions;
+using WorkoutWotch.UnitTests.Services.Delay.Mocks;
+using System.Collections.Generic;
+using Kent.Boogaart.PCLMock;
+using System.Threading;
+using System.Threading.Tasks;
+using WorkoutWotch.Models;
+using System.Linq;
+
+namespace WorkoutWotch.UnitTests.Models.Actions
+{
+    [TestFixture]
+    public class BreakActionFixture
+    {
+        [Test]
+        public void ctor_throws_if_delay_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new BreakAction(null, new SpeechServiceMock(), TimeSpan.FromSeconds(30)));
+        }
+
+        [Test]
+        public void ctor_throws_if_speech_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new BreakAction(new DelayServiceMock(), null, TimeSpan.FromSeconds(30)));
+        }
+
+        [Test]
+        public void ctor_throws_if_duration_is_less_than_zero()
+        {
+            Assert.Throws<ArgumentException>(() => new BreakAction(new DelayServiceMock(), new SpeechServiceMock(), TimeSpan.FromSeconds(-2)));
+        }
+
+        [Test]
+        public void duration_returns_value_provided_in_ctor()
+        {
+            Assert.AreEqual(TimeSpan.FromSeconds(9), new BreakAction(new DelayServiceMock(), new SpeechServiceMock(), TimeSpan.FromSeconds(9)).Duration);
+        }
+
+        [Test]
+        public void execute_async_throws_if_the_context_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(async () => await new BreakAction(new DelayServiceMock(), new SpeechServiceMock(), TimeSpan.FromSeconds(1)).ExecuteAsync(null));
+        }
+
+        [Test]
+        public async Task execute_async_composes_actions_appropriately_for_long_breaks()
+        {
+            var delayService = new DelayServiceMock();
+            var speechService = new SpeechServiceMock();
+            var performedActions = new List<string>();
+
+            delayService
+                .When(x => x.DelayAsync(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                .Do<TimeSpan, CancellationToken>((duration, ct) => performedActions.Add("Delayed for " + duration))
+                .Return(Task.FromResult(true));
+            speechService
+                .When(x => x.SpeakAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .Do<string, CancellationToken>((speechText, cty) => performedActions.Add("Saying '" + speechText + "'"))
+                .Return(Task.FromResult(true));
+
+            var sut = new BreakAction(delayService, speechService, TimeSpan.FromSeconds(5));
+
+            await sut.ExecuteAsync(new ExecutionContext());
+
+            Assert.AreEqual(7, performedActions.Count);
+            Assert.AreEqual("Saying 'break'", performedActions[0]);
+            Assert.AreEqual("Delayed for 00:00:01", performedActions[1]);
+            Assert.AreEqual("Delayed for 00:00:01", performedActions[2]);
+            Assert.AreEqual("Delayed for 00:00:01", performedActions[3]);
+            Assert.AreEqual("Saying 'ready?'", performedActions[4]);
+            Assert.AreEqual("Delayed for 00:00:01", performedActions[5]);
+            Assert.AreEqual("Delayed for 00:00:01", performedActions[6]);
+        }
+
+        [Test]
+        public async Task execute_async_composes_actions_appropriately_for_short_breaks()
+        {
+            var delayService = new DelayServiceMock();
+            var speechService = new SpeechServiceMock();
+            var performedActions = new List<string>();
+
+            delayService
+                .When(x => x.DelayAsync(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                .Do<TimeSpan, CancellationToken>((duration, ct) => performedActions.Add("Delayed for " + duration))
+                .Return(Task.FromResult(true));
+            speechService
+                .When(x => x.SpeakAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .Do<string, CancellationToken>((speechText, cty) => performedActions.Add("Saying '" + speechText + "'"))
+                .Return(Task.FromResult(true));
+
+            var sut = new BreakAction(delayService, speechService, TimeSpan.FromSeconds(1));
+
+            await sut.ExecuteAsync(new ExecutionContext());
+
+            Assert.AreEqual(2, performedActions.Count);
+            Assert.AreEqual("Saying 'break'", performedActions[0]);
+            Assert.AreEqual("Delayed for 00:00:01", performedActions[1]);
+        }
+    }
+}
+
