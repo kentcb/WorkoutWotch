b5008d61b6c3eb74fc518bc0951552851cfd9fd7
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-10 14:42:52 +1030

    Module 05, Video 21: Model: String Literal Parser

---
+++ Src/WorkoutWotch.Models/Parsers/StringLiteralParser.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Sprache;
+using System.Globalization;
+using System.Text;
+
+namespace WorkoutWotch.Models.Parsers
+{
+    internal static class StringLiteralParser
+    {
+        private static Parser<string> GetStringLiteralParser(char delimiter)
+        {
+            return i =>
+            {
+                if (i.AtEnd)
+                {
+                    return Result.Failure<string>(
+                        i,
+                        "unexpected end of input",
+                        new string[] { string.Format(CultureInfo.InvariantCulture, "string delimitered by {0}", delimiter) });
+                }
+
+                if (i.Current != delimiter)
+                {
+                    return Result.Failure<string>(
+                        i,
+                        string.Format(CultureInfo.InvariantCulture, "unexpected '{0}'", i.Current),
+                        new string[] { delimiter.ToString() });
+                }
+
+                var result = new StringBuilder();
+                var escaped = false;
+
+                while (true)
+                {
+                    i = i.Advance();
+
+                    if (i.AtEnd)
+                    {
+                        return Result.Failure<string>(
+                            i,
+                            "unexpected end of input",
+                            new string[] { string.Format(CultureInfo.InvariantCulture, "continued string contents or '{0}'", delimiter) });
+                    }
+
+                    if (i.Current == '\\')
+                    {
+                        if (escaped)
+                        {
+                            result.Append(i.Current);
+                            escaped = false;
+                            continue;
+                        }
+
+                        escaped = true;
+                    }
+                    else if (i.Current == delimiter)
+                    {
+                        if (escaped)
+                        {
+                            result.Append(i.Current);
+                            escaped = false;
+                            continue;
+                        }
+
+                        i = i.Advance();
+                        break;
+                    }
+                    else
+                    {
+                        escaped = false;
+                        result.Append(i.Current);
+                    }
+                }
+
+                return Result.Success(result.ToString(), i);
+            };
+        }
+
+        public static readonly Parser<string> Parser = GetStringLiteralParser('"')
+            .Or(GetStringLiteralParser('\''));
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Parsers/StringLiteralParserFixture.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.Models.Parsers;
+using Sprache;
+
+namespace WorkoutWotch.UnitTests.Models.Parsers
+{
+    [TestFixture]
+    public class StringLiteralParserFixture
+    {
+        [TestCase("''", "")]
+        [TestCase("'  '", "  ")]
+        [TestCase("'hello'", "hello")]
+        [TestCase("'world'", "world")]
+        [TestCase("'  hello, world!  '", "  hello, world!  ")]
+        public void can_parse_single_quoted_strings(string input, string expected)
+        {
+            var result = StringLiteralParser.Parser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(@"""""", "")]
+        [TestCase(@"""  """, "  ")]
+        [TestCase(@"""hello""", "hello")]
+        [TestCase(@"""world""", "world")]
+        [TestCase(@"""  hello, world!  """, "  hello, world!  ")]
+        public void can_parse_double_quoted_strings(string input, string expected)
+        {
+            var result = StringLiteralParser.Parser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(@"'hello \'friend\''", "hello 'friend'")]
+        [TestCase(@"""hello \""friend\""""", @"hello ""friend""")]
+        public void can_parse_escaped_strings(string input, string expected)
+        {
+            var result = StringLiteralParser.Parser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void cannot_parse_empty_input()
+        {
+            var result = StringLiteralParser.Parser(new Input(""));
+            Assert.False(result.WasSuccessful);
+            Assert.AreEqual("unexpected end of input", result.Message);
+        }
+
+        [TestCase("  ")]
+        [TestCase("hello")]
+        [TestCase("world")]
+        public void cannot_parse_non_delimitered_string(string input)
+        {
+            var result = StringLiteralParser.Parser(new Input(input));
+            Assert.False(result.WasSuccessful);
+            Assert.AreEqual("unexpected '" + input[0] + "'", result.Message);
+        }
+
+        [TestCase("'")]
+        [TestCase(@"""")]
+        [TestCase("'hello")]
+        [TestCase(@"""hello")]
+        public void cannot_parse_non_terminated_string(string input)
+        {
+            var result = StringLiteralParser.Parser(new Input(input));
+            Assert.False(result.WasSuccessful);
+            Assert.AreEqual("unexpected end of input", result.Message);
+        }
+    }
+}
+
