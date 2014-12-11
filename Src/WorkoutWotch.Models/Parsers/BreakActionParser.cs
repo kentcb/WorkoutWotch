8b398ea2c2e9f49377caca52be82fdcd9c3e0c2d
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-11 16:32:01 +1030

    Module 05, Video 26: Model: BreakActionParser

---
+++ Src/WorkoutWotch.Models/Parsers/BreakActionParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Sprache;
+using WorkoutWotch.Models.Actions;
+using WorkoutWotch.Services.Contracts.Delay;
+using WorkoutWotch.Services.Contracts.Speech;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models.Parsers
+{
+    internal static class BreakActionParser
+    {
+        public static Parser<BreakAction> GetParser(IDelayService delayService, ISpeechService speechService)
+        {
+            delayService.AssertNotNull("delayService");
+            speechService.AssertNotNull("speechService");
+
+            return
+                from _ in Parse.IgnoreCase("break")
+                from __ in Parse.WhiteSpace.AtLeastOnce()
+                from ___ in Parse.IgnoreCase("for")
+                from ____ in Parse.WhiteSpace.AtLeastOnce()
+                from duration in TimeSpanParser.Parser
+                select new BreakAction(delayService, speechService, duration);
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Parsers/BreakActionParserFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.UnitTests.Services.Speech.Mocks;
+using WorkoutWotch.Models.Parsers;
+using WorkoutWotch.UnitTests.Services.Delay.Mocks;
+using Sprache;
+
+namespace WorkoutWotch.UnitTests.Models.Parsers
+{
+    [TestFixture]
+    public class BreakActionParserFixture
+    {
+        private const int msInSecond = 1000;
+        private const int msInMinute = 60 * msInSecond;
+        private const int msInHour = 60 * msInMinute;
+
+        [Test]
+        public void get_parser_throws_if_delay_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => BreakActionParser.GetParser(null, new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_speech_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => BreakActionParser.GetParser(new DelayServiceMock(), null));
+        }
+
+        [TestCase("Break for 24s", 24 * msInSecond)]
+        [TestCase("Break for 1m 24s", 1 * msInMinute + 24 * msInSecond)]
+        [TestCase("break For 24s", 24 * msInSecond)]
+        [TestCase("Break   For \t 24s", 24 * msInSecond)]
+        public void can_parse_valid_input(string input, int expectedMilliseconds)
+        {
+            var result = BreakActionParser.GetParser(new DelayServiceMock(), new SpeechServiceMock()).Parse(input);
+            Assert.AreEqual(TimeSpan.FromMilliseconds(expectedMilliseconds), result.Duration);
+        }
+
+        [TestCase("Brake for 24s")]
+        [TestCase("Breakfor 24s")]
+        [TestCase("Break for")]
+        [TestCase("Break for tea")]
+        public void cannot_parse_invalid_input(string input)
+        {
+            var result = BreakActionParser.GetParser(new DelayServiceMock(), new SpeechServiceMock())(new Input(input));
+            Assert.False(result.WasSuccessful);
+        }
+    }
+}
+
