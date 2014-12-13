1541c66c279d268ce5ae48672377b445e88a6731
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-13 11:18:22 +1030

    Module 05, Video 30: Model: ActionListParser

---
+++ Src/WorkoutWotch.Models/Parsers/ActionListParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using WorkoutWotch.Services.Contracts.Audio;
+using WorkoutWotch.Services.Contracts.Delay;
+using WorkoutWotch.Services.Contracts.Logger;
+using WorkoutWotch.Services.Contracts.Speech;
+using Sprache;
+using System.Collections.Generic;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models.Parsers
+{
+    internal static class ActionListParser
+    {
+        public static Parser<IEnumerable<IAction>> GetParser(
+            int indentLevel,
+            IAudioService audioService,
+            IDelayService delayService,
+            ILoggerService loggerService,
+            ISpeechService speechService)
+        {
+            if (indentLevel < 0)
+            {
+                throw new ArgumentException("indentLevel must be greater than or equal to 0.", "indentLevel");
+            }
+
+            audioService.AssertNotNull("audioService");
+            delayService.AssertNotNull("delayService");
+            loggerService.AssertNotNull("loggerService");
+            speechService.AssertNotNull("speechService");
+
+            return
+                (from _ in Parse.String(new string(' ', indentLevel * 2))
+                 from __ in Parse.String("* ")
+                 from action in ActionParser.GetParser(indentLevel, audioService, delayService, loggerService, speechService)
+                 from ___ in Parse.WhiteSpace.Except(NewLineParser.Parser).Many()
+                 select action).DelimitedBy(NewLineParser.Parser);
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Parsers/ActionListParserFixture.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using WorkoutWotch.UnitTests.Services.Audio.Mocks;
+using WorkoutWotch.Models.Parsers;
+using WorkoutWotch.UnitTests.Services.Delay.Mocks;
+using WorkoutWotch.UnitTests.Services.Logger.Mocks;
+using WorkoutWotch.UnitTests.Services.Speech.Mocks;
+using Kent.Boogaart.PCLMock;
+using Sprache;
+using WorkoutWotch.Models.Actions;
+
+namespace WorkoutWotch.UnitTests.Models.Parsers
+{
+    [TestFixture]
+    public class ActionListParserFixture
+    {
+        [Test]
+        public void get_parser_throws_if_indent_level_is_less_than_zero()
+        {
+            Assert.Throws<ArgumentException>(() => ActionListParser.GetParser(-1, new AudioServiceMock(), new DelayServiceMock(), new LoggerServiceMock(), new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_audio_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ActionListParser.GetParser(0, null, new DelayServiceMock(), new LoggerServiceMock(), new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_delay_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ActionListParser.GetParser(0, new AudioServiceMock(), null, new LoggerServiceMock(), new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_logger_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ActionListParser.GetParser(0, new AudioServiceMock(), new DelayServiceMock(), null, new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_speech_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ActionListParser.GetParser(0, new AudioServiceMock(), new DelayServiceMock(), new LoggerServiceMock(), null));
+        }
+
+        [TestCase(
+            "* Wait for 2s\n* Break for 1m",
+            new [] { typeof(WaitAction), typeof(BreakAction) })]
+        [TestCase(
+            "* Wait for 2s  \n* Break for 1m   \t  ",
+            new [] { typeof(WaitAction), typeof(BreakAction) })]
+        [TestCase(
+            "* Say 'foo'\n* Wait for 1s\n* Wait for 2s\n* Break for 1m",
+            new [] { typeof(SayAction), typeof(WaitAction), typeof(WaitAction), typeof(BreakAction) })]
+        public void can_parse_valid_input(string input, Type[] expectedActionTypes)
+        {
+            var result = ActionListParser
+                .GetParser(0, new AudioServiceMock(), new DelayServiceMock(), new LoggerServiceMock(MockBehavior.Loose), new SpeechServiceMock())
+                .Parse(input);
+
+            Assert.NotNull(result);
+            Assert.True(result.Select(x => x.GetType()).SequenceEqual(expectedActionTypes));
+        }
+
+        [TestCase("")]
+        [TestCase("* ")]
+        [TestCase("Wait for 2s")]
+        [TestCase(" * Wait for 2s")]
+        public void cannot_parse_invalid_input(string input)
+        {
+            var result = ActionListParser
+                .GetParser(0, new AudioServiceMock(), new DelayServiceMock(), new LoggerServiceMock(MockBehavior.Loose), new SpeechServiceMock())(new Input(input));
+
+            Assert.False(result.WasSuccessful);
+        }
+    }
+}
+
