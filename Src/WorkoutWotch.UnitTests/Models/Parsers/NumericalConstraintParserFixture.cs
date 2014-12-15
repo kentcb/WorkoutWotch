44505cbc82c38eafa6f7ee7df8adeb1c048fefaf
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-15 19:06:35 +1030

    Improving tests, particularly around negative parsing cases.

--- Src/WorkoutWotch.UnitTests/Models/Parsers/NumericalConstraintParserFixture.cs
+++ Src/WorkoutWotch.UnitTests/Models/Parsers/NumericalConstraintParserFixture.cs
@@ -15,7 +15,7 @@
         [TestCase("2", 3, 1, 4, false)]
         public void can_parse_literal_constraints(string input, int actual, int first, int last, bool expected)
         {
-            var result = NumericalConstraintParser.GetParser(e => actual, e => first, e => last).Parse(input);
+            var result = NumericalConstraintParser.GetParser(ec => actual, ec => first, ec => last).Parse(input);
             Assert.AreEqual(expected, result(new ExecutionContext()));
         }
 
@@ -29,7 +29,7 @@
         [TestCase("LAST", 4, 1, 4, true)]
         public void can_parse_symbol_constraints(string input, int actual, int first, int last, bool expected)
         {
-            var result = NumericalConstraintParser.GetParser(e => actual, e => first, e => last).Parse(input);
+            var result = NumericalConstraintParser.GetParser(ec => actual, ec => first, ec => last).Parse(input);
             Assert.AreEqual(expected, result(new ExecutionContext()));
         }
 
@@ -45,7 +45,7 @@
         [TestCase("last-1", 4, 1, 4, false)]
         public void can_parse_symbol_expression_constraints(string input, int actual, int first, int last, bool expected)
         {
-            var result = NumericalConstraintParser.GetParser(e => actual, e => first, e => last).Parse(input);
+            var result = NumericalConstraintParser.GetParser(ec => actual, ec => first, ec => last).Parse(input);
             Assert.AreEqual(expected, result(new ExecutionContext()));
         }
 
@@ -59,6 +59,12 @@
         [TestCase("1..2..5", 3, 1, 5, true)]
         [TestCase("1..2..5", 4, 1, 5, false)]
         [TestCase("1..2..5", 5, 1, 5, true)]
+        [TestCase("2..2..5", 2, 1, 5, true)]
+        [TestCase("2..2..5", 3, 1, 5, false)]
+        [TestCase("1..3..5", 1, 1, 5, true)]
+        [TestCase("1..3..5", 2, 1, 5, false)]
+        [TestCase("1..3..5", 3, 1, 5, false)]
+        [TestCase("1..3..5", 4, 1, 5, true)]
         [TestCase("first..2..last", 4, 1, 5, false)]
         [TestCase("first..2..last", 5, 1, 5, true)]
         [TestCase("first..2..last-1", 5, 1, 5, false)]
@@ -66,7 +72,7 @@
         [TestCase("2..3", 1, 1, 4, false)]
         public void can_parse_range_constraints(string input, int actual, int first, int last, bool expected)
         {
-            var result = NumericalConstraintParser.GetParser(e => actual, e => first, e => last).Parse(input);
+            var result = NumericalConstraintParser.GetParser(ec => actual, ec => first, ec => last).Parse(input);
             Assert.AreEqual(expected, result(new ExecutionContext()));
         }
 
@@ -82,7 +88,7 @@
         [TestCase("1 ,  2 \t , \t  3", 3, 1, 4, true)]
         public void can_parse_mathematical_set_constraints(string input, int actual, int first, int last, bool expected)
         {
-            var result = NumericalConstraintParser.GetParser(e => actual, e => first, e => last).Parse(input);
+            var result = NumericalConstraintParser.GetParser(ec => actual, ec => first, ec => last).Parse(input);
             Assert.AreEqual(expected, result(new ExecutionContext()));
         }
 
@@ -104,8 +110,24 @@
         [TestCase("^  \t first  + 1  .. 2  \t ..  last", 1, 1, 10, true)]
         public void can_parse_not_constraints(string input, int actual, int first, int last, bool expected)
         {
-            var result = NumericalConstraintParser.GetParser(e => actual, e => first, e => last).Parse(input);
+            var result = NumericalConstraintParser.GetParser(ec => actual, ec => first, ec => last).Parse(input);
             Assert.AreEqual(expected, result(new ExecutionContext()));
+        }
+
+        [TestCase("")]
+        [TestCase("frist")]
+        [TestCase("1..\n3")]
+        [TestCase("first,\nlast")]
+        [TestCase("first\n,last")]
+        [TestCase("^\n3")]
+        [TestCase("first+\n1")]
+        [TestCase("last-\n1")]
+        [TestCase("first-1")]
+        [TestCase("last+1")]
+        public void cannot_parse_invalid_input(string input)
+        {
+            var result = NumericalConstraintParser.GetParser(ec => 0, ec => 0, ec => 0)(new Input(input));
+            Assert.True(!result.WasSuccessful || !result.Remainder.AtEnd);
         }
     }
 }
