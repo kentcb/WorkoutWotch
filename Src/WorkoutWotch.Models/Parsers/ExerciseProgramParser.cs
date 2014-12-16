deb3e7dfeaa14fcd350452f258feb4edfce56ad4
Kent Boogaart <kent.boogaart@gmail.com>
2014-12-16 19:53:40 +1030

    Module 05, Video 35: Model: ExerciseProgramParser

---
+++ Src/WorkoutWotch.Models/Parsers/ExerciseProgramParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Sprache;
+using WorkoutWotch.Services.Contracts.Audio;
+using WorkoutWotch.Services.Contracts.Delay;
+using WorkoutWotch.Services.Contracts.Logger;
+using WorkoutWotch.Services.Contracts.Speech;
+using System.Linq;
+using Kent.Boogaart.HelperTrinity.Extensions;
+
+namespace WorkoutWotch.Models.Parsers
+{
+    internal static class ExerciseProgramParser
+    {
+        private static readonly Parser<string> nameParser =
+            from _ in Parse.String("#")
+            from name in Parse.AnyChar.Until(NewLineParser.Parser)
+            select new string(name.ToArray()).Trim();
+
+        public static Parser<ExerciseProgram> GetParser(IAudioService audioService, IDelayService delayService, ILoggerService loggerService, ISpeechService speechService)
+        {
+            audioService.AssertNotNull("audioService");
+            delayService.AssertNotNull("delayService");
+            loggerService.AssertNotNull("loggerService");
+            speechService.AssertNotNull("speechService");
+
+            return
+                from name in nameParser
+                from exercises in ExerciseParser.GetParser(audioService, delayService, loggerService, speechService).DelimitedBy(Parse.WhiteSpace.Many()).Optional()
+                select new ExerciseProgram(loggerService, name, exercises.GetOrElse(Enumerable.Empty<Exercise>()));
+        }
+    }
+}
+
---
+++ Src/WorkoutWotch.UnitTests/Models/Parsers/ExerciseProgramParserFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using WorkoutWotch.Models.Parsers;
+using WorkoutWotch.UnitTests.Services.Delay.Mocks;
+using WorkoutWotch.UnitTests.Services.Logger.Mocks;
+using WorkoutWotch.UnitTests.Services.Speech.Mocks;
+using WorkoutWotch.UnitTests.Services.Audio.Mocks;
+using Kent.Boogaart.PCLMock;
+using Sprache;
+
+namespace WorkoutWotch.UnitTests.Models.Parsers
+{
+    [TestFixture]
+    public class ExerciseProgramParserFixture
+    {
+        [Test]
+        public void get_parser_throws_if_audio_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ExerciseProgramParser.GetParser(null, new DelayServiceMock(), new LoggerServiceMock(), new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_delay_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ExerciseProgramParser.GetParser(new AudioServiceMock(), null, new LoggerServiceMock(), new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_logger_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ExerciseProgramParser.GetParser(new AudioServiceMock(), new DelayServiceMock(), null, new SpeechServiceMock()));
+        }
+
+        [Test]
+        public void get_parser_throws_if_speech_service_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => ExerciseProgramParser.GetParser(new AudioServiceMock(), new DelayServiceMock(), new LoggerServiceMock(), null));
+        }
+
+        [TestCase("# foo\n", "foo")]
+        [TestCase("# Foo\n", "Foo")]
+        [TestCase("# Foo bar\n", "Foo bar")]
+        [TestCase("#    \t Foo   bar  \t \n", "Foo   bar")]
+        public void can_parse_name(string input, string expectedName)
+        {
+            var result = ExerciseProgramParser
+                .GetParser(new AudioServiceMock(), new DelayServiceMock(), new LoggerServiceMock(MockBehavior.Loose), new SpeechServiceMock())
+                .Parse(input);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(expectedName, result.Name);
+        }
+
+        [TestCase("# ignore\n", 0)]
+        [TestCase("# ignore\n## Exercise 1\n* 1 set x 1 rep\n", 1)]
+        [TestCase("# ignore\n## Exercise 1\n* 1 set x 1 rep\n## Exercise 2\n* 1 set x 1 rep\n", 2)]
+        [TestCase("# ignore\n## Exercise 1\n* 1 set x 1 rep\n\n\n\n## Exercise 2\n* 1 set x 1 rep\n", 2)]
+        public void can_parse_exercises(string input, int expectedExerciseCount)
+        {
+            var result = ExerciseProgramParser
+                .GetParser(new AudioServiceMock(), new DelayServiceMock(), new LoggerServiceMock(MockBehavior.Loose), new SpeechServiceMock())
+                .Parse(input);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(expectedExerciseCount, result.Exercises.Count);
+        }
+    }
+}
+
