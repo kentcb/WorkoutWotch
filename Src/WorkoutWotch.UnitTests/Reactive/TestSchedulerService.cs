35d41a429f180adaa0ea00bbc9d071e31a7d7c29
Kent Boogaart <kent.boogaart@gmail.com>
2015-04-04 18:19:06 +1030

    Improved TestSchedulerService. See http://stackoverflow.com/questions/28494641/techniques-for-testing-code-that-uses-multiple-schedulers.

--- Src/WorkoutWotch.UnitTests/Reactive/TestSchedulerService.cs
+++ Src/WorkoutWotch.UnitTests/Reactive/TestSchedulerService.cs
@@ -1,47 +1,110 @@
 ﻿namespace WorkoutWotch.UnitTests.Reactive
 {
     using System;
+    using System.Collections.Generic;
     using System.Reactive.Concurrency;
     using System.Reactive.Linq;
-    using WorkoutWotch.Services.Contracts.Scheduler;
     using Microsoft.Reactive.Testing;
+    using WorkoutWotch.Services.Contracts.Scheduler;
 
-    public sealed class TestSchedulerService : TestScheduler, ISchedulerService
+    public sealed class TestSchedulerService : ISchedulerService
     {
-        public IScheduler DefaultScheduler
+        private readonly bool hasSingleUnderlyingScheduler;
+        private readonly TestScheduler defaultScheduler;
+        private readonly TestScheduler currentThreadScheduler;
+        private readonly TestScheduler immediateScheduler;
+        private readonly TestScheduler synchronizationContextScheduler;
+        private readonly TestScheduler eventLoopScheduler;
+        private readonly TestScheduler newThreadScheduler;
+        private readonly TestScheduler taskPoolScheduler;
+
+        public TestSchedulerService(bool singleUnderlyingScheduler = true)
         {
-            get { return this; }
+            this.hasSingleUnderlyingScheduler = singleUnderlyingScheduler;
+            this.defaultScheduler = new TestScheduler();
+
+            if (singleUnderlyingScheduler)
+            {
+                this.currentThreadScheduler =
+                    this.immediateScheduler =
+                    this.synchronizationContextScheduler =
+                    this.eventLoopScheduler =
+                    this.newThreadScheduler =
+                    this.taskPoolScheduler =
+                    this.defaultScheduler;
+            }
+            else
+            {
+                this.currentThreadScheduler = new TestScheduler();
+                this.immediateScheduler = new TestScheduler();
+                this.synchronizationContextScheduler = new TestScheduler();
+                this.eventLoopScheduler = new TestScheduler();
+                this.newThreadScheduler = new TestScheduler();
+                this.taskPoolScheduler = new TestScheduler();
+            }
         }
 
-        public IScheduler CurrentThreadScheduler
-        {
-            get { return this; }
-        }
+        public IScheduler DefaultScheduler => this.defaultScheduler;
 
-        public IScheduler ImmediateScheduler
-        {
-            get { return this; }
-        }
+        public IScheduler CurrentThreadScheduler => this.currentThreadScheduler;
 
-        public IScheduler SynchronizationContextScheduler
-        {
-            get { return this; }
-        }
+        public IScheduler ImmediateScheduler => this.immediateScheduler;
 
-        public IScheduler TaskPoolScheduler
-        {
-            get { return this; }
-        }
+        public IScheduler SynchronizationContextScheduler => this.synchronizationContextScheduler;
+
+        public IScheduler EventLoopScheduler => this.eventLoopScheduler;
+
+        public IScheduler NewThreadScheduler => this.newThreadScheduler;
+
+        public IScheduler TaskPoolScheduler => this.taskPoolScheduler;
 
         public IDisposable Pump()
-        {
-            return Pump(TimeSpan.FromMilliseconds(10));
-        }
+            => Pump(TimeSpan.FromMilliseconds(10));
 
         // useful hack to allow tests to automatically pump any scheduled items
         public IDisposable Pump(TimeSpan frequency)
+            => Observable.Timer(TimeSpan.Zero, frequency).Subscribe(_ => this.Start());
+
+        public void Start()
         {
-            return Observable.Timer(TimeSpan.Zero, frequency).Subscribe(_ => this.Start());
+            foreach (var testScheduler in this.GetTestSchedulers())
+            {
+                testScheduler.Start();
+            }
+        }
+
+        public void AdvanceBy(long time)
+        {
+            foreach (var testScheduler in this.GetTestSchedulers())
+            {
+                testScheduler.AdvanceBy(time);
+            }
+        }
+
+        public void AdvanceTo(long time)
+        {
+            foreach (var testScheduler in this.GetTestSchedulers())
+            {
+                testScheduler.AdvanceTo(time);
+            }
+        }
+
+        private IEnumerable<TestScheduler> GetTestSchedulers()
+        {
+            yield return this.defaultScheduler;
+
+            if (this.hasSingleUnderlyingScheduler)
+            {
+                yield break;
+            }
+
+            yield return this.defaultScheduler;
+            yield return this.currentThreadScheduler;
+            yield return this.immediateScheduler;
+            yield return this.synchronizationContextScheduler;
+            yield return this.eventLoopScheduler;
+            yield return this.newThreadScheduler;
+            yield return this.taskPoolScheduler;
         }
     }
 }
