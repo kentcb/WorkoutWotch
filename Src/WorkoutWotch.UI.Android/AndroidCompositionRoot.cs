d8218b088f4a7c00ae42f552266215571686eee7
Kent Boogaart <kent.boogaart@gmail.com>
2015-12-29 10:15:26 +1030

    Use canned document service by default.

--- Src/WorkoutWotch.UI.Android/AndroidCompositionRoot.cs
+++ Src/WorkoutWotch.UI.Android/AndroidCompositionRoot.cs
@@ -22,11 +22,11 @@
 
         protected override IExerciseDocumentService CreateExerciseDocumentService() =>
             // just used canned data - useful for getting you up and running quickly
-            //new CannedExerciseDocumentService();
+            new CannedExerciseDocumentService();
             // comment the above lines and uncomment this line if you want to use an iCloud-based document service
-            new GoogleDriveExerciseDocumentService(
-                this.loggerService.Value,
-                this.mainActivity);
+            //new GoogleDriveExerciseDocumentService(
+            //    this.loggerService.Value,
+            //    this.mainActivity);
 
         protected override ISpeechService CreateSpeechService() =>
             new SpeechService();
